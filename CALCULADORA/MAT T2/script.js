7930b832b7a98736c6443813f9698697c698a9ac
Marcos Dessa (UCSal) <marcosdessa.oliveira@ucsal.edu.br>
2024-03-25 16:42:47 -0300

    Matutino T2: projeto básico concluído

--- CALCULADORA/MAT T2/script.js
+++ CALCULADORA/MAT T2/script.js
@@ -6,6 +6,7 @@ class Calculadora {
         this.iniciouSegundo = false;
         this.memTemp = '';
         this.estadoErro = false;
+        this.memoria = 0;
         this.op = {
             NOP: 0,
             DIV: 1,
@@ -14,15 +15,22 @@ class Calculadora {
             SUM: 4
         };
         this.opAtual = this.op.NOP;
-        // PRECISA DE MAIS ATRIBUTOS?
     }
 
     mostrarVisor() {
+        if (this.estadoErro) {
+            this.nrVisor = '0';
+            return 'ERRO!';
+        }
+        if (this.nrVisor.length == 0) {
+            this.nrVisor = '0';
+        }
         return this.nrVisor;
     }
 
     // recebe dígito
     digito(dig) {
+        if (this.estadoErro) return;
         if (dig.length != 1) return;
         if ((dig < '0' || dig > '9') && dig != '.') return;
         if (!this.iniciouSegundo && this.opAtual != this.op.NOP) {
@@ -44,6 +52,7 @@ class Calculadora {
 
     // Definir qual a operação atual
     defineOperacao(op) {
+        if (this.estadoErro) return;
         switch (op) {
             case '+':
                 this.opAtual = this.op.SUM;
@@ -63,12 +72,17 @@ class Calculadora {
 
     // Executa operação: tecla IGUAL
     igual() {
+        if (this.estadoErro) return;
+        if (this.opAtual == this.op.NOP) return;
         let num1 = parseFloat(this.memTemp);
         let num2 = parseFloat(this.nrVisor);
         let resultado = 0;
         switch (this.opAtual) {
             case this.op.DIV:
-                // PERIGO: DIV POR ZERO POSSÍVEL !!!!!
+                if (num2 == 0) {
+                    this.estadoErro = true;
+                    return;
+                }
                 resultado = num1/num2;
                 break;
             case this.op.MULT:
@@ -86,7 +100,6 @@ class Calculadora {
         this.ptDecimal = false;
         this.memTemp = '';
         this.nrVisor = String(resultado).slice(0, 10);
-        console.log('operação atual:', this.opAtual)
     }
 
     // Limpa dados (exceto memória)
@@ -97,6 +110,30 @@ class Calculadora {
         this.opAtual = this.op.NOP;
         this.memTemp = '';
         this.estadoErro = false;
+    }
+
+    // tecla M+ : acrescenta à memória o número no visor
+    teclaMmais() {
+        if (this.estadoErro) return;
+        this.memoria += parseFloat(this.nrVisor);
+    }
+
+    // tecla M- : subtrai da memória o número no visor
+    teclaMmenos() {
+        if (this.estadoErro) return;
+        this.memoria -= parseFloat(this.nrVisor);
+    }
+
+    // tecla RM : recupera o conteúdo da memória -> coloca no visor
+    teclaRM() {
+        if (this.estadoErro) return;
+        this.nrVisor = String(this.memoria);
+    }
+
+    // tecla CLM : limpa totalmente o conteúdo da memória -> atribui 0
+    teclaCLM() {
+        if (this.estadoErro) return;
+        this.memoria = 0;
     }
 
 }
@@ -125,15 +162,37 @@ let defOp = (op) => {
     calculadora.defineOperacao(op);
 }
 
-// Calcula a operação
+// CALCULA A OPERAÇÃO
 let defIgual = () => {
     calculadora.igual();
     atualizaVisor();
 }
 
+// TECLA C: LIMPA TUDO, EXCETO MEMÓRIA
 let teclaC = () => {
     calculadora.teclaC();
     atualizaVisor();
+}
+
+// M+ ACRESCENTA À MEMÓRIA O NÚMERO ATUAL NO VISOR
+let teclaMmais = () => {
+    calculadora.teclaMmais();
+}
+
+// M- SUBTRAI DA MEMÓRIA O NÚMERO ATUAL NO VISOR
+let teclaMmenos = () => {
+    calculadora.teclaMmenos();
+}
+
+// PÕE NO VISOR O CONTEÚDO DA MEMÓRIA
+let teclaRM = () => {
+    calculadora.teclaRM();
+    atualizaVisor();
+}
+
+// APAGA TODO O CONTEÚDO DA MEMÓRIA
+let teclaCLM = () => {
+    calculadora.teclaCLM();
 }
 
 // ========================================================
