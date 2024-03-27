40ae0c085800cf1daf63aa4a55d0dc989d92a556
Marcos Dessa (UCSal) <marcosdessa.oliveira@ucsal.edu.br>
2024-03-27 15:04:34 -0300

    Matutino T3: projeto básico concluído

--- CALCULADORA/MAT T3/script.js
+++ CALCULADORA/MAT T3/script.js
@@ -3,6 +3,17 @@ class Calculadora {
     constructor() {
         this.nrVisor = '0';
         this.ptDecimal = false;
+        this.iniciouSegundo = false;
+        this.estadoErro = false;
+        this.memTemp = '';
+        this.op = {
+            NOP: 0,
+            DIV: 1,
+            MULT: 2,
+            SUB: 3,
+            SUM: 4
+        };
+        this.opAtual = this.op.NOP;
         // EXISTEM OUTRO ATRIBUTOS?
 
     }
@@ -14,21 +25,87 @@ class Calculadora {
 
     // Recebe um dígito
     insereDigito(dig) {
+        if (this.estadoErro) return;
         if (dig.length != 1) return;
-        if (this.nrVisor.length == 10) return;
         if ((dig < '0' || dig > '9') && dig != '.') return;
+        if (!this.iniciouSegundo && this.opAtual != this.op.NOP) {
+            this.iniciouSegundo = true;
+            this.ptDecimal = false;
+            this.nrVisor = '0';
+        }
         if (dig == '.') {
             if (this.ptDecimal) return;
             this.ptDecimal = true;
         }
+        if (this.nrVisor.length == 10) return;
         if (this.nrVisor == '0') {
             this.nrVisor = dig == '.' ? '0.' : dig;
         } else {
             this.nrVisor += dig;
         }
-        
     }
 
+    // Define a operação atual
+    defineOperacao(op) {
+        if (this.estadoErro) return;
+        switch (op) {
+            case '+':
+                this.opAtual = this.op.SUM;
+                break;
+            case '-':
+                this.opAtual = this.op.SUB;
+                break;
+            case '*':
+                this.opAtual = this.op.MULT;
+                break;
+            case '/':
+                this.opAtual = this.op.DIV;
+                break;
+        }
+        this.memTemp = this.nrVisor;
+    }
+
+    igual() {
+        if (this.estadoErro) return;
+        let num1 = parseFloat(this.memTemp);
+        let num2 = parseFloat(this.nrVisor);
+        let resultado = 0;
+        switch (this.opAtual) {
+            case this.op.SUM:
+                resultado = num1 + num2;
+                break;
+            case this.op.SUB:
+                resultado = num1 - num2;
+                break;
+            case this.op.MULT:
+                resultado = num1 * num2;
+                break;
+            case this.op.DIV:
+                // PERIGO: DIVISÃO POR ZERO
+                if (num2 == 0) {
+                    this.estadoErro = true;
+                    this.nrVisor = 'ERRO!';
+                    return;
+                }
+                resultado = num1 / num2;
+        }
+        this.opAtual = this.op.NOP;
+        this.ptDecimal = false;
+        this.iniciouSegundo = false;
+        this.memTemp = '';
+        this.nrVisor = String(resultado).slice(0, 10);
+    }
+
+    // Limpa o conteúdo do visor e as operações (mas não a memória)
+    teclaC() {
+        this.nrVisor = '0';
+        this.ptDecimal = false;
+        this.memTemp = '';
+        this.iniciouSegundo = false;
+        this.estadoErro = false;
+        this.opAtual = this.op.NOP;
+    }
+    
 }
 
 
@@ -47,6 +124,24 @@ let digito = (dig) => {
     atualizaVisor();
 }
 
+let defineOp = (op) => {
+    if (calculadora.opAtual != calculadora.op.NOP) {
+        igual();
+        atualizaVisor();
+    }
+    calculadora.defineOperacao(op);
+}
+
+let igual = () => {
+    calculadora.igual();
+    atualizaVisor();
+}
+
+let teclaC = () => {
+    calculadora.teclaC();
+    atualizaVisor();
+}
+
 
 
 
@@ -54,5 +149,4 @@ let digito = (dig) => {
 
 // ==========  INICIALIZAÇÃO ===================
 let calculadora = new Calculadora();
-// calculadora.nrVisor = '3493';
-// atualizaVisor();
+
--- CALCULADORA/NOT T1/script.js
+++ CALCULADORA/NOT T1/script.js
@@ -3,6 +3,17 @@ class Calculadora {
     constructor() {
         this.nrVisor = '0';
         this.ptDecimal = false;
+        this.estadoErro = false;
+        this.memTemp = '';
+        this.iniciouSegundo = false;
+        this.op = {
+            NOP: 0,
+            SUM: 1,
+            SUB: 2,
+            MULT: 3,
+            DIV: 4
+        };
+        this.opAtual = this.op.NOP;
         // TEM MAIS ATRIBUTOS?
     }
 
@@ -13,14 +24,77 @@ class Calculadora {
 
     // Recebe dígito
     recebeDigito(dig) {
+        if (this.estadoErro) return;
         if (dig.length != 1) return;
-        if (this.nrVisor.length == 10) return;
-        // PROVISÓRIO
         if ((dig < '0' || dig > '9') && dig != '.') return;
-        if (this.nrVisor == '0') {
-            
+        if (!this.iniciouSegundo && this.opAtual != this.op.NOP) {
+            this.iniciouSegundo = true;
+            this.ptDecimal = false;
+            this.nrVisor = '0';
         }
-        this.nrVisor += dig;
+        if (this.nrVisor.length == 10) return;
+        if (dig == '.') {
+            if (this.ptDecimal) return;
+            this.ptDecimal = true;
+        }
+        if (this.nrVisor == '0') {
+            this.nrVisor = dig == '.' ? '0.' : dig;
+        } else {
+            this.nrVisor += dig;
+        }
+        console.log('numero: ', this.nrVisor)
+    }
+
+    // Define a operação atual
+    defineOperacao(op) {
+        if (this.estadoErro) return;
+        switch (op) {
+            case '+':
+                this.opAtual = this.op.SUM;
+                break;
+            case '-':
+                this.opAtual = this.op.SUB;
+                break;
+            case '/':
+                this.opAtual = this.op.DIV;
+                break;
+            case '*':
+                this.opAtual = this.op.MULT;
+                break;
+        }
+        this.memTemp = this.nrVisor;
+    }
+
+    igual() {
+        if (this.estadoErro) return;
+        if (this.opAtual == this.op.NOP) return;
+        let num1 = parseFloat(this.memTemp);
+        let num2 = parseFloat(this.nrVisor);
+        let resultado = 0;
+        switch (this.opAtual) {
+            case this.op.SUM:
+                resultado = num1 + num2;
+                break;
+            case this.op.SUB:
+                resultado = num1 - num2;
+                break;
+            case this.op.MULT:
+                resultado = num1 * num2;
+                break;
+            case this.op.DIV:
+                if (num2 == 0) {
+                    this.estadoErro = true;
+                    this.nrVisor = 'ERRO!';
+                    return;
+                }
+                resultado = num1 / num2;
+                break;
+        }
+        this.opAtual = this.op.NOP;
+        this.ptDecimal = false;
+        this.memTemp = '';
+        this.iniciouSegundo = false;
+        this.nrVisor = String(resultado).slice(0, 10);
     }
 
 }
@@ -28,10 +102,6 @@ class Calculadora {
 // ===================================================================
 //  REAÇÃO A EVENTOS DO MOUSE
 // ===================================================================
-
-let calculadora = new Calculadora();
-
-
 
 
 let mostraVisor = () => {
@@ -42,5 +112,28 @@ let digito = (dig) => {
     calculadora.recebeDigito(dig);
     mostraVisor();
 }
+
+let defOp = (op) => {
+    if (calculadora.opAtual != calculadora.op.NOP) {
+        igual();
+        mostraVisor();
+    }
+    calculadora.defineOperacao(op);
+}
+
+let igual = () => {
+    calculadora.igual();
+    mostraVisor();
+}
+
+
+
+
+
+
+
+
+let calculadora = new Calculadora();
+
 
 
